15783530a7e6f70b392c96220d9746b6af3f2baa
yousuke12 <nakagawa_y@digicomnet.co.jp>
2026-04-23 10:03:13 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+const today = new Date();
+const year = today.getUTCFullYear();
+const month = today.getUTCMonth();
+const day = today.getUTCDate();
+
+console.log(new Date());
+console.log(today.getUTCFullYear());
+console.log(today.getUTCMonth() + 1);
+console.log(today.getUTCDate());
