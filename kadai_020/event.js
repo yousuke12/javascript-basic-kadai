4c16da8f19b4f37fc700c67bb8c86d76acd35120
yousuke12 <nakagawa_y@digicomnet.co.jp>
2026-04-23 13:42:47 +0900

    Add files via upload

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -2,5 +2,7 @@ const Btn = document.getElementById('btn');
 
 Btn.addEventListener('click', () => {
   // テキストボックスに入力された文字列を取得する
-document.getElementById('text').innerText = 'ボタンをクリックしました';
+const Text = document.getElementById('text');
+
+Text.innerText = 'ボタンをクリックしました';
 });
